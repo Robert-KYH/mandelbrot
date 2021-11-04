e475757a25ac0d3d426156545c1057bcbfc7cf85
Robert <Robert.Leffmann@student.kyh.se>
2021-11-04 22:18:37 +0100

    Focus and zoom in on mouse left-click

--- src/Application.java
+++ src/Application.java
@@ -2,10 +2,12 @@ import javax.swing.JFrame;
 import java.awt.Rectangle;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
 
 class Application implements Runnable {
 
-  //private double centerX = 0, centerY = 0, zoomFactor = 2;
+  private double centerX = -0.5, centerY = 0, zoomFactor = 4;
+  private ImagePanel panel;
   //private RenderManager manager = new RenderManager();
 
   public void run() {
@@ -14,15 +16,37 @@ class Application implements Runnable {
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     Rectangle r = frame.getGraphicsConfiguration().getBounds();
-    ImagePanel panel = new ImagePanel(r.width*4/5, r.height*4/5);
-    //panel.addMouseListener(new MouseAdapter() {  public void mouseClicked(MouseEvent e) {  click(e);  }  });
+    panel = new ImagePanel(r.width*4/5, r.height*4/5);
+    render();
+    panel.addMouseListener(new MouseAdapter() {  public void mouseClicked(MouseEvent e) {  click(e);  }  });
 
     frame.add(panel);
     frame.pack();
     frame.setVisible(true);
   }
-  /*
+
   private void click(MouseEvent e) {
+    int w = panel.width, h = panel.height;
+    centerX = centerX+(e.getX()-w/2)*zoomFactor/w;
+    centerY = centerY+(e.getY()-h/2)*zoomFactor/w;
+    zoomFactor /= 3;
+    render();
+    panel.repaint();
   }
-  */
+
+  private void render() {
+    int w = panel.width, h = panel.height;
+    BufferedImage img = panel.image;
+
+    for (int y = 0; y < h; ++y) {
+      for (int x = 0; x < w; ++x) {
+        Complex z = new Complex(centerX+(x-w/2)*zoomFactor/w, centerY+(y-h/2)*zoomFactor/w);
+        Complex z0 = new Complex(z);
+        for (int i = 0; i++ < 100; ) z = z.sqr().plus(z0);
+        int c = z.mag() < 2 ? 0x404040 : 0xb0b0b0;
+        img.setRGB(x, y, c);
+      }
+    }
+
+  }
 }
--- src/ImagePanel.java
+++ src/ImagePanel.java
@@ -5,23 +5,15 @@ import java.awt.Dimension;
 
 
 class ImagePanel extends JPanel {
-  private BufferedImage image;
+  BufferedImage image;
+  int width, height;
 
   ImagePanel(int w, int h) {
     super();
+    width = w;
+    height = h;
     setPreferredSize(new Dimension(w, h));
     image = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
-
-    for (int y = 0; y < h; ++y) {
-      for (int x = 0; x < w; ++x) {
-        Complex z = new Complex(x*0.003-2, y*0.003-1);
-        Complex z0 = new Complex(z);
-        for (int i = 0; i++ < 100; ) z = z.sqr().plus(z0);
-        int c = z.mag() < 2 ? 0x404040 : 0xb0b0b0;
-        image.setRGB(x, y, c);
-      }
-    }
-
   }
 
   @Override public void paintComponent(Graphics g) {  g.drawImage(image, 0, 0, null);  }
